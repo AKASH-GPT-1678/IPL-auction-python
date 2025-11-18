0f26f3e233a8d99578cb35d80da32d5f7b38cb9a
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-11-18 20:41:48 +0530

    dhoni auction

--- playerstats.py
+++ playerstats.py
@@ -1,21 +1,66 @@
 import json
+fields = [
+    "name",
+    "role",
+    "bowling_stats",
+    "batting_stats",
+    "fielding_stats",
+    "base_price",
+    "sale_price"
+]
 
-def updateBowlstats(name, bowling_stats):
+def update_player_field():
+    name = input("Enter player name to update: ")
+    field_name = input("Enter fieldname ?")
+    if field_name not in fields:
+        print("Enter Valid Field")
+        return
+        
+            
+    value = input(f"Enter new value for {field_name}: ")
+
    
     with open('data/players.json', "r") as f:
         player_list = json.load(f)
 
- 
-    for data in player_list:
-        if data["name"] == name:     
-            data["bowling_stats"] = bowling_stats
+  
+    updated = False
+    for player in player_list:
+        if player["name"] == name:
+            player[field_name] = value
+            updated = True
+            break
 
+   
+    if updated:
+        with open('data/players.json', "w") as f:
+            json.dump(player_list, f, indent=4)
+        print(f"{field_name} updated successfully!")
+    else:
+        print("Player not found!")
+
+
+
+def deletePlayer():
+    name = input("Player you want to delete? ")
+    
+
+    with open('data/players.json', "r") as f:
+        player_list = json.load(f)
+    
+ 
+    for player in player_list:
+        if player["name"] == name:
+            player_list.remove(player)
+            print("Player Deleted Successfully")
+            break
+    else:
+        print("Player not found!")
+    
     
     with open('data/players.json', "w") as f:
         json.dump(player_list, f, indent=4)
 
+        
 
-
-
-
-updateBowlstats("Ms Dhoni" , "200" )
+update_player_field()
